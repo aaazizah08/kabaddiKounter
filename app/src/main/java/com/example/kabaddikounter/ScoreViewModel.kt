aac70f49872b41fbb4fd5b5d51c763bf1c6642d3
aaazizah08 <nurazizahpa53@gmail.com>
2025-03-06 00:29:50 +0700

    Parameter untuk menambah skor salah satu tim

--- app/src/main/java/com/example/kabaddikounter/ScoreViewModel.kt
+++ app/src/main/java/com/example/kabaddikounter/ScoreViewModel.kt
@@ -5,4 +5,12 @@ import androidx.lifecycle.ViewModel
 class ScoreViewModel:ViewModel() {
     var scoreA: Int = 0
     var scoreB: Int = 0
+
+    fun incrementScore(isTeamA: Boolean){
+        if (isTeamA) {
+            scoreA++
+        } else {
+            scoreB++
+        }
+    }
 }
