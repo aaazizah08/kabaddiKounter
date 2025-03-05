a489e997dbcf930eb7868f90611683d8d320263b
aaazizah08 <nurazizahpa53@gmail.com>
2025-03-06 00:47:55 +0700

    Memperbarui kode agar angka tidak berubah saat di rotasi

--- app/src/main/java/com/example/kabaddikounter/MainActivity.kt
+++ app/src/main/java/com/example/kabaddikounter/MainActivity.kt
@@ -10,6 +10,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel: ScoreViewModel by viewModels()
+
+    private lateinit var tvScoreA: TextView
+    private lateinit var tvScoreB: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,10 +25,46 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        tvScoreA = findViewById(R.id.scoreA)
+        tvScoreB = findViewById(R.id.scoreB)
         val buttonPlusOneA = findViewById<Button>(R.id.buttonPlusOneA)
         val buttonPlusTwoA = findViewById<Button>(R.id.buttonPlusTwoA)
         val buttonPlusOneB = findViewById<Button>(R.id.buttonPlusOneB)
         val buttonPlusTwoB = findViewById<Button>(R.id.buttonPlusTwoB)
         val buttonReset = findViewById<Button>(R.id.buttonReset)
+
+        updateScoreUI()
+
+        buttonPlusOneA.setOnClickListener {
+            viewModel.incrementScore(true)
+            updateScoreUI()
+        }
+
+        buttonPlusTwoA.setOnClickListener {
+            viewModel.incrementScore(true)
+            viewModel.incrementScore(true)
+            updateScoreUI()
+        }
+
+        buttonPlusOneB.setOnClickListener {
+            viewModel.incrementScore(false)
+            updateScoreUI()
+        }
+
+        buttonPlusTwoB.setOnClickListener {
+            viewModel.incrementScore(false)
+            viewModel.incrementScore(false)
+            updateScoreUI()
+        }
+
+        buttonReset.setOnClickListener {
+            viewModel.resetScore()
+            updateScoreUI()
+        }
+    }
+
+    private fun updateScoreUI() {
+        tvScoreA.text = viewModel.scoreA.toString()
+        tvScoreB.text = viewModel.scoreB.toString()
     }
 }
