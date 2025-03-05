5fc776c0cf16aff8e19694f728c4a3118ef1d5fa
aaazizah08 <nurazizahpa53@gmail.com>
2025-03-06 00:45:38 +0700

    Menghubungkan tombol agar bisa berfungsi

--- app/src/main/java/com/example/kabaddikounter/MainActivity.kt
+++ app/src/main/java/com/example/kabaddikounter/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.kabaddikounter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,5 +19,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val buttonPlusOneA = findViewById<Button>(R.id.buttonPlusOneA)
+        val buttonPlusTwoA = findViewById<Button>(R.id.buttonPlusTwoA)
+        val buttonPlusOneB = findViewById<Button>(R.id.buttonPlusOneB)
+        val buttonPlusTwoB = findViewById<Button>(R.id.buttonPlusTwoB)
+        val buttonReset = findViewById<Button>(R.id.buttonReset)
     }
 }
